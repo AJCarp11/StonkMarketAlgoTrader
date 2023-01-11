441328a4e84642e2c6b05daa3af837f9e5fb2f8b
Andy <andrewcarper@outlook.com>
2023-01-11 13:32:04 -0500

    Hello World

---
+++ AlgoTrader/src/runTrader.java
@@ -0,0 +1,21 @@
+/**
+ * 
+ */
+
+/**
+ * @author Andy
+ *
+ */
+public class runTrader {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		System.out.println("Hello World");
+		
+	}
+
+}
